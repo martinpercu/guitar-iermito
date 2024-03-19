5906a1192f10c57c8d5bdaab6a8802344403d857
Martin Mendez <martinpercu@gmail.com>
2024-03-19 16:07:01 -0400

    added firebase config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"victoriermito","appId":"1:120603141811:web:5d721c5704b797c0a972c6","storageBucket":"victoriermito.appspot.com","apiKey":"AIzaSyDi0aeXA_Mfl1uygE7i2sn8OBlsHJ1l2Ko","authDomain":"victoriermito.firebaseapp.com","messagingSenderId":"120603141811"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
