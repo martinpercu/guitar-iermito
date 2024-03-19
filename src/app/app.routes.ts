4f12d0ea85e09de5c23db1a287038af949c20e31
Martin Mendez <martinpercu@gmail.com>
2024-03-19 16:02:22 -0400

    starting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { GuitarsComponent } from './domains/qr/guitars/guitars.component';
+
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: GuitarsComponent
+  },
+  // {
+  //   path: 'admin',
+  //   component: ClientsFullComponent
+  // }
+];
