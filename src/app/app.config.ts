167fc227640305119590d0bc45ef285b922569a9
Martin Mendez <martinpercu@gmail.com>
2024-03-23 19:38:28 -0400

    hide the api vars

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
 
+import { environment } from '../environment/environment';
+
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
@@ -9,7 +11,7 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"victoriermito","appId":"1:120603141811:web:5d721c5704b797c0a972c6","storageBucket":"victoriermito.appspot.com","apiKey":"AIzaSyDi0aeXA_Mfl1uygE7i2sn8OBlsHJ1l2Ko","authDomain":"victoriermito.firebaseapp.com","messagingSenderId":"120603141811"}))),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment))),
     importProvidersFrom(provideAuth(() => getAuth())),
     importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
