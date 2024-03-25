948cda70f3868aa2b79ef84528207a6563b589b5
Martin Mendez <martinpercu@gmail.com>
2024-03-25 17:01:32 -0400

    add svg logo + nav bar + minor css for guitar show for users

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'base': '#787976',
+        'strong': '#484a46',
+        'muted': '#c1cace',
+        'brand': '#cda433',
+        'brandreverse': '#b81918',
+        'brand-strong': '#eabe38',
+        'brandreverse-strong': '#7f1710',
+      }
+    },
   },
   plugins: [],
 }
